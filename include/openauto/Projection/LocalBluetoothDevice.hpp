f5f364d904645b7f324406fe83148cafbdc6df5c
Robert Crowley <robert5974@gmail.com>
2025-12-14 13:43:30 -0500

    rewrite to ensure fix is applied correctly

--- include/openauto/Projection/LocalBluetoothDevice.hpp
+++ include/openauto/Projection/LocalBluetoothDevice.hpp
@@ -16,44 +16,43 @@
 *  along with openauto. If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include <QBluetoothLocalDevice>
 #include "IBluetoothDevice.hpp"
+#include <QBluetoothLocalDevice>
+
 
 #pragma once
 
-namespace openauto
-{
-namespace projection
-{
+namespace openauto {
+namespace projection {
 
-class LocalBluetoothDevice: public QObject, public IBluetoothDevice
-{
-    Q_OBJECT
+class LocalBluetoothDevice : public QObject, public IBluetoothDevice {
+  Q_OBJECT
 
 public:
-    LocalBluetoothDevice();
+  LocalBluetoothDevice();
 
-    void stop() override;
-    bool isPaired(const std::string& address) const override;
-    void pair(const std::string& address, PairingPromise::Pointer promise) override;
-    std::string getLocalAddress() const override;
-    bool isAvailable() const override;
+  void stop() override;
+  bool isPaired(const std::string &address) const override;
+  void pair(const std::string &address,
+            PairingPromise::Pointer promise) override;
+  std::string getLocalAddress() const override;
+  bool isAvailable() const override;
 
 signals:
-    void startPairing(const QString& address, PairingPromise::Pointer promise);
+  void startPairing(const QString &address, PairingPromise::Pointer promise);
 
 private slots:
-    void createBluetoothLocalDevice();
-    void onStartPairing(const QString& address, PairingPromise::Pointer promise);
-    void onError(QBluetoothLocalDevice::Error error);
-    void onHostModeStateChanged(QBluetoothLocalDevice::HostMode state);
+  void createBluetoothLocalDevice();
+  void onStartPairing(const QString &address, PairingPromise::Pointer promise);
+  void onError(QBluetoothLocalDevice::Error error);
+  void onHostModeStateChanged(QBluetoothLocalDevice::HostMode state);
 
 private:
-    mutable std::mutex mutex_;
-    std::unique_ptr<QBluetoothLocalDevice> localDevice_;
-    PairingPromise::Pointer pairingPromise_;
-    QBluetoothAddress pairingAddress_;
+  mutable std::mutex mutex_;
+  std::unique_ptr<QBluetoothLocalDevice> localDevice_;
+  PairingPromise::Pointer pairingPromise_;
+  QBluetoothAddress pairingAddress_;
 };
 
-}
-}
+} // namespace projection
+} // namespace openauto
