edbea12554f25b74f9ce7a08b095af673f507594
Robert Crowley <robert5974@gmail.com>
2025-12-14 21:25:36 -0500

    migrated from QAudioOutput to QAudioSink and updated the format handling

--- include/openauto/Projection/QtAudioOutput.hpp
+++ include/openauto/Projection/QtAudioOutput.hpp
@@ -18,48 +18,49 @@
 
 #pragma once
 
-#include <QAudioOutput>
-#include <QAudioFormat>
 #include "IAudioOutput.hpp"
 #include "SequentialBuffer.hpp"
+#include <QAudioFormat>
+#include <QAudioSink>
+#include <QMediaDevices>
 
-namespace openauto
-{
-namespace projection
-{
 
-class QtAudioOutput: public QObject, public IAudioOutput
-{
-    Q_OBJECT
+namespace openauto {
+namespace projection {
+
+class QtAudioOutput : public QObject, public IAudioOutput {
+  Q_OBJECT
 
 public:
-    QtAudioOutput(uint32_t channelCount, uint32_t sampleSize, uint32_t sampleRate);
-    bool open() override;
-    void write(aasdk::messenger::Timestamp::ValueType, const aasdk::common::DataConstBuffer& buffer) override;
-    void start() override;
-    void stop() override;
-    void suspend() override;
-    uint32_t getSampleSize() const override;
-    uint32_t getChannelCount() const override;
-    uint32_t getSampleRate() const override;
+  QtAudioOutput(uint32_t channelCount, uint32_t sampleSize,
+                uint32_t sampleRate);
+  bool open() override;
+  void write(aasdk::messenger::Timestamp::ValueType,
+             const aasdk::common::DataConstBuffer &buffer) override;
+  void start() override;
+  void stop() override;
+  void suspend() override;
+  uint32_t getSampleSize() const override;
+  uint32_t getChannelCount() const override;
+  uint32_t getSampleRate() const override;
 
 signals:
-    void startPlayback();
-    void suspendPlayback();
-    void stopPlayback();
+  void startPlayback();
+  void suspendPlayback();
+  void stopPlayback();
 
 protected slots:
-    void createAudioOutput();
-    void onStartPlayback();
-    void onSuspendPlayback();
-    void onStopPlayback();
+  void createAudioOutput();
+  void onStartPlayback();
+  void onSuspendPlayback();
+  void onStopPlayback();
 
 private:
-    QAudioFormat audioFormat_;
-    SequentialBuffer audioBuffer_;
-    std::unique_ptr<QAudioOutput> audioOutput_;
-    bool playbackStarted_;
+  QAudioFormat audioFormat_;
+  SequentialBuffer audioBuffer_;
+  std::unique_ptr<QAudioSink> audioOutput_;
+  bool playbackStarted_;
 };
 
-}
-}
+} // namespace projection
+} // namespace openauto
--- openauto/Projection/QtAudioOutput.cpp
+++ openauto/Projection/QtAudioOutput.cpp
@@ -16,106 +16,88 @@
 *  along with openauto. If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include <QApplication>
 #include "openauto/Projection/QtAudioOutput.hpp"
 #include "OpenautoLog.hpp"
+#include <QApplication>
 
+namespace openauto {
+namespace projection {
 
-namespace openauto
-{
-namespace projection
-{
+QtAudioOutput::QtAudioOutput(uint32_t channelCount, uint32_t sampleSize,
+                             uint32_t sampleRate)
+    : playbackStarted_(false) {
+  audioFormat_.setChannelCount(channelCount);
+  audioFormat_.setSampleRate(sampleRate);
 
-QtAudioOutput::QtAudioOutput(uint32_t channelCount, uint32_t sampleSize, uint32_t sampleRate)
-    : playbackStarted_(false)
-{
-    audioFormat_.setChannelCount(channelCount);
-    audioFormat_.setSampleRate(sampleRate);
-    audioFormat_.setSampleSize(sampleSize);
-    audioFormat_.setCodec("audio/pcm");
-    audioFormat_.setByteOrder(QAudioFormat::LittleEndian);
-    audioFormat_.setSampleType(QAudioFormat::SignedInt);
+  if (sampleSize == 8)
+    audioFormat_.setSampleFormat(QAudioFormat::UInt8);
+  else if (sampleSize == 16)
+    audioFormat_.setSampleFormat(QAudioFormat::Int16);
+  else if (sampleSize == 32)
+    audioFormat_.setSampleFormat(QAudioFormat::Int32);
+  else
+    audioFormat_.setSampleFormat(QAudioFormat::Int16); // Default fallback
 
-    this->moveToThread(QApplication::instance()->thread());
-    connect(this, &QtAudioOutput::startPlayback, this, &QtAudioOutput::onStartPlayback);
-    connect(this, &QtAudioOutput::suspendPlayback, this, &QtAudioOutput::onSuspendPlayback);
-    connect(this, &QtAudioOutput::stopPlayback, this, &QtAudioOutput::onStopPlayback);
+  this->moveToThread(QApplication::instance()->thread());
+  connect(this, &QtAudioOutput::startPlayback, this,
+          &QtAudioOutput::onStartPlayback);
+  connect(this, &QtAudioOutput::suspendPlayback, this,
+          &QtAudioOutput::onSuspendPlayback);
+  connect(this, &QtAudioOutput::stopPlayback, this,
+          &QtAudioOutput::onStopPlayback);
 
-    QMetaObject::invokeMethod(this, "createAudioOutput", Qt::BlockingQueuedConnection);
+  QMetaObject::invokeMethod(this, "createAudioOutput",
+                            Qt::BlockingQueuedConnection);
 }
 
-void QtAudioOutput::createAudioOutput()
-{
-    OPENAUTO_LOG(debug) << "[QtAudioOutput] create.";
-    audioOutput_ = std::make_unique<QAudioOutput>(QAudioDeviceInfo::defaultOutputDevice(), audioFormat_);
+void QtAudioOutput::createAudioOutput() {
+  OPENAUTO_LOG(debug) << "[QtAudioOutput] create.";
+  audioOutput_ = std::make_unique<QAudioSink>(
+      QMediaDevices::defaultAudioOutput(), audioFormat_);
 }
 
-bool QtAudioOutput::open()
-{
-    return audioBuffer_.open(QIODevice::ReadWrite);
+bool QtAudioOutput::open() { return audioBuffer_.open(QIODevice::ReadWrite); }
+
+void QtAudioOutput::write(aasdk::messenger::Timestamp::ValueType,
+                          const aasdk::common::DataConstBuffer &buffer) {
+  audioBuffer_.write(reinterpret_cast<const char *>(buffer.cdata), buffer.size);
 }
 
-void QtAudioOutput::write(aasdk::messenger::Timestamp::ValueType, const aasdk::common::DataConstBuffer& buffer)
-{
-    audioBuffer_.write(reinterpret_cast<const char*>(buffer.cdata), buffer.size);
+void QtAudioOutput::start() { emit startPlayback(); }
+
+void QtAudioOutput::stop() { emit stopPlayback(); }
+
+void QtAudioOutput::suspend() { emit suspendPlayback(); }
+
+uint32_t QtAudioOutput::getSampleSize() const {
+  return audioFormat_.bytesPerSample() * 8;
 }
 
-void QtAudioOutput::start()
-{
-    emit startPlayback();
+uint32_t QtAudioOutput::getChannelCount() const {
+  return audioFormat_.channelCount();
 }
 
-void QtAudioOutput::stop()
-{
-    emit stopPlayback();
+uint32_t QtAudioOutput::getSampleRate() const {
+  return audioFormat_.sampleRate();
 }
 
-void QtAudioOutput::suspend()
-{
-    emit suspendPlayback();
+void QtAudioOutput::onStartPlayback() {
+  if (!playbackStarted_) {
+    audioOutput_->start(&audioBuffer_);
+    playbackStarted_ = true;
+  } else {
+    audioOutput_->resume();
+  }
 }
 
-uint32_t QtAudioOutput::getSampleSize() const
-{
-    return audioFormat_.sampleSize();
+void QtAudioOutput::onSuspendPlayback() { audioOutput_->suspend(); }
+
+void QtAudioOutput::onStopPlayback() {
+  if (playbackStarted_) {
+    audioOutput_->stop();
+    playbackStarted_ = false;
+  }
 }
 
-uint32_t QtAudioOutput::getChannelCount() const
-{
-    return audioFormat_.channelCount();
-}
-
-uint32_t QtAudioOutput::getSampleRate() const
-{
-    return audioFormat_.sampleRate();
-}
-
-void QtAudioOutput::onStartPlayback()
-{
-    if(!playbackStarted_)
-    {
-        audioOutput_->start(&audioBuffer_);
-        playbackStarted_ = true;
-    }
-    else
-    {
-        audioOutput_->resume();
-    }
-}
-
-void QtAudioOutput::onSuspendPlayback()
-{
-    audioOutput_->suspend();
-}
-
-void QtAudioOutput::onStopPlayback()
-{
-    if(playbackStarted_)
-    {
-        audioOutput_->stop();
-        playbackStarted_ = false;
-    }
-}
-
-}
-}
+} // namespace projection
+} // namespace openauto
