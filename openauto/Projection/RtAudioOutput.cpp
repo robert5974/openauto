c9e622aaec207c0df939173029cfb1c5e4ba3905
Robert Crowley <robert5974@gmail.com>
2025-12-14 20:42:05 -0500

    applied the fixes for RtAudioOutput

--- openauto/Projection/RtAudioOutput.cpp
+++ openauto/Projection/RtAudioOutput.cpp
@@ -19,132 +19,108 @@
 #include "openauto/Projection/RtAudioOutput.hpp"
 #include "OpenautoLog.hpp"
 
-namespace openauto
-{
-namespace projection
-{
+namespace openauto {
+namespace projection {
 
-RtAudioOutput::RtAudioOutput(uint32_t channelCount, uint32_t sampleSize, uint32_t sampleRate)
-    : channelCount_(channelCount)
-    , sampleSize_(sampleSize)
-    , sampleRate_(sampleRate)
-{
-    std::vector<RtAudio::Api> apis;
-    RtAudio::getCompiledApi(apis);
-    dac_ = std::find(apis.begin(), apis.end(), RtAudio::LINUX_PULSE) == apis.end() ? std::make_unique<RtAudio>() : std::make_unique<RtAudio>(RtAudio::LINUX_PULSE);
+RtAudioOutput::RtAudioOutput(uint32_t channelCount, uint32_t sampleSize,
+                             uint32_t sampleRate)
+    : channelCount_(channelCount), sampleSize_(sampleSize),
+      sampleRate_(sampleRate) {
+  std::vector<RtAudio::Api> apis;
+  RtAudio::getCompiledApi(apis);
+  dac_ = std::find(apis.begin(), apis.end(), RtAudio::LINUX_PULSE) == apis.end()
+             ? std::make_unique<RtAudio>()
+             : std::make_unique<RtAudio>(RtAudio::LINUX_PULSE);
 }
 
-bool RtAudioOutput::open()
-{
-    std::lock_guard<decltype(mutex_)> lock(mutex_);
+bool RtAudioOutput::open() {
+  std::lock_guard<decltype(mutex_)> lock(mutex_);
 
-    if(dac_->getDeviceCount() > 0)
-    {
-        RtAudio::StreamParameters parameters;
-        parameters.deviceId = dac_->getDefaultOutputDevice();
-        parameters.nChannels = channelCount_;
-        parameters.firstChannel = 0;
+  if (dac_->getDeviceCount() > 0) {
+    RtAudio::StreamParameters parameters;
+    parameters.deviceId = dac_->getDefaultOutputDevice();
+    parameters.nChannels = channelCount_;
+    parameters.firstChannel = 0;
 
-        try
-        {
-            RtAudio::StreamOptions streamOptions;
-            streamOptions.flags = RTAUDIO_MINIMIZE_LATENCY | RTAUDIO_SCHEDULE_REALTIME;
-            uint32_t bufferFrames = sampleRate_ == 16000 ? 1024 : 2048; //according to the observation of audio packets
-            dac_->openStream(&parameters, nullptr, RTAUDIO_SINT16, sampleRate_, &bufferFrames, &RtAudioOutput::audioBufferReadHandler, static_cast<void*>(this), &streamOptions);
-            return audioBuffer_.open(QIODevice::ReadWrite);
-        }
-        catch(const RtAudioError& e)
-        {
-            OPENAUTO_LOG(error) << "[RtAudioOutput] Failed to open audio output, what: " << e.what();
-        }
+    RtAudio::StreamOptions streamOptions;
+    streamOptions.flags = RTAUDIO_MINIMIZE_LATENCY | RTAUDIO_SCHEDULE_REALTIME;
+    uint32_t bufferFrames =
+        sampleRate_ == 16000
+            ? 1024
+            : 2048; // according to the observation of audio packets
+    if (dac_->openStream(&parameters, nullptr, RTAUDIO_SINT16, sampleRate_,
+                         &bufferFrames, &RtAudioOutput::audioBufferReadHandler,
+                         static_cast<void *>(this), &streamOptions) != 0) {
+      OPENAUTO_LOG(error) << "[RtAudioOutput] Failed to open audio output: "
+                          << dac_->getErrorText();
+      return false;
     }
-    else
-    {
-        OPENAUTO_LOG(error) << "[RtAudioOutput] No output devices found.";
+    return audioBuffer_.open(QIODevice::ReadWrite);
+  } else {
+    OPENAUTO_LOG(error) << "[RtAudioOutput] No output devices found.";
+  }
+
+  return false;
+}
+
+void RtAudioOutput::write(aasdk::messenger::Timestamp::ValueType timestamp,
+                          const aasdk::common::DataConstBuffer &buffer) {
+  audioBuffer_.write(reinterpret_cast<const char *>(buffer.cdata), buffer.size);
+}
+
+void RtAudioOutput::start() {
+  std::lock_guard<decltype(mutex_)> lock(mutex_);
+
+  if (dac_->isStreamOpen() && !dac_->isStreamRunning()) {
+    if (dac_->startStream() != 0) {
+      OPENAUTO_LOG(error) << "[RtAudioOutput] Failed to start audio output: "
+                          << dac_->getErrorText();
     }
-
-    return false;
+  }
 }
 
-void RtAudioOutput::write(aasdk::messenger::Timestamp::ValueType timestamp, const aasdk::common::DataConstBuffer& buffer)
-{
-    audioBuffer_.write(reinterpret_cast<const char*>(buffer.cdata), buffer.size);
+void RtAudioOutput::stop() {
+  std::lock_guard<decltype(mutex_)> lock(mutex_);
+
+  this->doSuspend();
+
+  if (dac_->isStreamOpen()) {
+    dac_->closeStream();
+  }
 }
 
-void RtAudioOutput::start()
-{
-    std::lock_guard<decltype(mutex_)> lock(mutex_);
+void RtAudioOutput::suspend() {
+  // not needed
+}
 
-    if(dac_->isStreamOpen() && !dac_->isStreamRunning())
-    {
-        try
-        {
-            dac_->startStream();
-        }
-        catch(const RtAudioError& e)
-        {
-            OPENAUTO_LOG(error) << "[RtAudioOutput] Failed to start audio output, what: " << e.what();
-        }
+uint32_t RtAudioOutput::getSampleSize() const { return sampleSize_; }
+
+uint32_t RtAudioOutput::getChannelCount() const { return channelCount_; }
+
+uint32_t RtAudioOutput::getSampleRate() const { return sampleRate_; }
+
+void RtAudioOutput::doSuspend() {
+  if (dac_->isStreamOpen() && dac_->isStreamRunning()) {
+    if (dac_->stopStream() != 0) {
+      OPENAUTO_LOG(error) << "[RtAudioOutput] Failed to suspend audio output: "
+                          << dac_->getErrorText();
     }
+  }
 }
 
-void RtAudioOutput::stop()
-{
-    std::lock_guard<decltype(mutex_)> lock(mutex_);
+int RtAudioOutput::audioBufferReadHandler(void *outputBuffer, void *inputBuffer,
+                                          unsigned int nBufferFrames,
+                                          double streamTime,
+                                          RtAudioStreamStatus status,
+                                          void *userData) {
+  RtAudioOutput *self = static_cast<RtAudioOutput *>(userData);
+  std::lock_guard<decltype(self->mutex_)> lock(self->mutex_);
 
-    this->doSuspend();
-
-    if(dac_->isStreamOpen())
-    {
-        dac_->closeStream();
-    }
+  const auto bufferSize =
+      nBufferFrames * (self->sampleSize_ / 8) * self->channelCount_;
+  self->audioBuffer_.read(reinterpret_cast<char *>(outputBuffer), bufferSize);
+  return 0;
 }
 
-void RtAudioOutput::suspend()
-{
-    //not needed
-}
-
-uint32_t RtAudioOutput::getSampleSize() const
-{
-    return sampleSize_;
-}
-
-uint32_t RtAudioOutput::getChannelCount() const
-{
-    return channelCount_;
-}
-
-uint32_t RtAudioOutput::getSampleRate() const
-{
-    return sampleRate_;
-}
-
-void RtAudioOutput::doSuspend()
-{
-    if(dac_->isStreamOpen() && dac_->isStreamRunning())
-    {
-        try
-        {
-            dac_->stopStream();
-        }
-        catch(const RtAudioError& e)
-        {
-            OPENAUTO_LOG(error) << "[RtAudioOutput] Failed to suspend audio output, what: " << e.what();
-        }
-    }
-}
-
-int RtAudioOutput::audioBufferReadHandler(void* outputBuffer, void* inputBuffer, unsigned int nBufferFrames,
-                                          double streamTime, RtAudioStreamStatus status, void* userData)
-{
-    RtAudioOutput* self = static_cast<RtAudioOutput*>(userData);
-    std::lock_guard<decltype(self->mutex_)> lock(self->mutex_);
-
-    const auto bufferSize = nBufferFrames * (self->sampleSize_ / 8) * self->channelCount_;
-    self->audioBuffer_.read(reinterpret_cast<char*>(outputBuffer), bufferSize);
-    return 0;
-}
-
-}
-}
+} // namespace projection
+} // namespace openauto
